8ed8f1c84076b86f305ba3ae31e89c911a10e9d7
xdorijanx <dorijan11@gmail.com>
2019-09-18 17:08:46 +0200

    removed components

--- src/Containers/Layout/Layout.js
+++ src/Containers/Layout/Layout.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react'
-import Navigation from '../../Components/Navigation/Navigation'
-import { HashRouter, Route } from 'react-router-dom'
-
+import React, { Component } from "react";
+import Navigation from "../../Components/Navigation/Navigation";
+import DorijanCard from "../../Components/DorijanCard/DorijanCard";
+import { HashRouter, Route } from "react-router-dom";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "Open Sans"
+  }
+});
 export default class Layout extends Component {
-    render() {
-        return (
-            <div>
-                <HashRouter>
-                    <Route path='/' component={Navigation}/>
-                </HashRouter>
-           
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <HashRouter>
+          <ThemeProvider theme={theme}>
+            
+          </ThemeProvider>
+        </HashRouter>
+      </div>
+    );
+  }
 }
